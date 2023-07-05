d72f559174093d6f92478da7e951b2bf6156c734
Raphael Monteiro de Oliveira Rebello <raphaelsiqueiraduarte@gmail.com>
2023-07-05 13:17:18 -0300

    Criação dos arquivos SQLs

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+SELECT * FROM Cliente
+SELECT * FROM Distribuidor
+SELECT * FROM Fidelidade
+SELECT * FROM Forma_Pagamento
+SELECT * FROM Item_Pedido_Compra
+SELECT * FROM Item_Pedido_Venda
+SELECT * FROM Marca
+SELECT * FROM Pedido_Compra
+SELECT * FROM Pedido_Venda
+SELECT * FROM Produto
+
